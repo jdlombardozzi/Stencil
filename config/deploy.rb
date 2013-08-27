d928ad07edb01c611b3d1c5f9937cbf7425b5767
Jason Lombardozzi <jason.lombardozzi@gmail.com>
2013-08-27 14:45:26 -0400

    Update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ require 'rvm/capistrano'
 
 server 'tofurkey.gditweb.com', :app, :web, :db, :primary => true
 set :user, 'ioweb'
+set :use_sudo, false
 set :deploy_to, '/home/ioweb/apps/stencil'
 set :repository, 'git@git:stencil'
 set :tag_timestamp, Time.now.utc
@@ -16,3 +17,6 @@ set :keep_releases, 3
 
 set :rvm_ruby_string, '2.0.0@stencil'
 set :rvm_type, :user # this is the money config, it defaults to :system
+
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true # prevent sudo tty errors if using sudo
