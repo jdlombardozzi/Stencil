5ce0e14b9d01b04bacebdf4f6f1d3d998a2ebdf8
Jason Lombardozzi <jason.lombardozzi@gmail.com>
2013-10-04 16:04:05 -0400

    Add some implementation of the partials with actual data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,27 @@ class PagesController < ApplicationController
   # Dynamically checks for a method related to the page id and then renders the page
   # Will mostly be show through the index iframe
   def show
-    send("page__#{params[:id]}") if respond_to?("page__#{params[:id]}")
+    send("page__#{params[:id].gsub("\/", '_')}") if respond_to?("page__#{params[:id].gsub("\/", '_')}")
 
     @id = params[:id]
 
     render :layout => false
   end
 
-  def page__pages; end
+  def page__taxa_article
+    @article = Gditweb::Feature.new({date: Date.today.strftime('%Y-%m-%d'),
+                                     title: 'Lorem ipsum headline slipsum'
+                                    })
+
+    # Use @article to create an array of articles
+    @features_horizontal = 4.times.try(:inject, []) { |rows, row|
+      rows << @article
+
+      rows
+    }
+  end
+
+  def page__templates_article
+    @article = Gditweb::Feature.find('ste/feature/1')
+  end
 end
